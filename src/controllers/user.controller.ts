a1054b69a04109ecef22d63963fc6154992872a2
Hanz Chiziterem <66650409+hanzchiziterem@users.noreply.github.com>
2025-06-19 03:43:34 +0100

    Add the signin and signout controller.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import prisma from "../prisma/client";
 import { generateToken } from "../utils/jwt";
-import { hashPassword } from "../utils/hash";
+import { hashPassword, verifyPassword } from "../utils/hash";
 
 export const signup = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -18,3 +18,30 @@ export const signup = async (req: Request, res: Response) => {
   const token = generateToken(user.id);
   res.status(201).json({ token });
 };
+
+export const signin = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const isValid = await verifyPassword(password, user.password);
+  if (!isValid) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const token = generateToken(user.id);
+  res.status(200).json({ token });
+};
+
+export const getProfile = (req: Request, res: Response) => {
+  res.status(200).json({ user: req.user });
+};
+
+export const signout = async (req: Request, res: Response) => {
+  res
+    .clearCookie("jwt")
+    .status(200)
+    .json({ success: true, message: "Logged out successfully." });
+};
+
